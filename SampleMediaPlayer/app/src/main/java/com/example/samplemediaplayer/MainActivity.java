e06f6d9beaa97f7b8e43ea0f3570e24a4bd16c77
hd1801 <hardikdhawan0108@gmail.com>
2021-04-11 23:21:09 +0530

    updated mediaplayer app

--- SampleMediaPlayer/app/src/main/java/com/example/samplemediaplayer/MainActivity.java
+++ SampleMediaPlayer/app/src/main/java/com/example/samplemediaplayer/MainActivity.java
@@ -9,6 +9,8 @@ import android.widget.TextView;
 
 import org.w3c.dom.Text;
 
+import java.util.function.LongConsumer;
+
 public class MainActivity extends AppCompatActivity {
 
     int songID;
@@ -19,23 +21,40 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         songID=R.raw.song;
+        if(a!=null)
+        a.release();
         a= MediaPlayer.create(this,songID);
         state = (TextView)findViewById(R.id.state_textview);
     }
     public void playSong(View view)
     {
+        if(a!=null)
+            a.release();
         a.start();
         state.setText("Playing");
-
+        a.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+            @Override
+            public void onCompletion(MediaPlayer mp) {
+                state.setText("finished");
+            }
+        });
     }
     public void pauseSong(View view)
     {
         a.pause();
-        state.setText("paused");}
+        state.setText("paused");
+    }
     public void stopSong(View view)
     {
         a.stop();
         state.setText("idle");
-        a= MediaPlayer.create(this,songID);
+        if(a!=null)
+        a.release();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        a.stop();
     }
 }
