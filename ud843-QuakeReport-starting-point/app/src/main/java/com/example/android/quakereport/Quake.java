557e1e46d31341b8df05fed3415573ad216b6e52
hd1801 <hardikdhawan0108@gmail.com>
2021-04-25 23:10:05 +0530

    added quake app

---
+++ ud843-QuakeReport-starting-point/app/src/main/java/com/example/android/quakereport/Quake.java
@@ -0,0 +1,29 @@
+package com.example.android.quakereport;
+
+public class Quake {
+
+
+    String mMagnitude,mLocation,mDate;
+
+
+
+    public Quake(String mMagnitude, String mLocation, String mDate) {
+        this.mMagnitude = mMagnitude;
+        this.mLocation = mLocation;
+        this.mDate = mDate;
+    }
+
+
+    public String getmMagnitude() {
+        return mMagnitude;
+    }
+
+    public String getmLocation() {
+        return mLocation;
+    }
+
+    public String getmDate() {
+        return mDate;
+    }
+
+}
---
+++ ud843-QuakeReport-starting-point/app/src/main/java/com/example/android/quakereport/QuakeAdapter.java
@@ -0,0 +1,21 @@
+package com.example.android.quakereport;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import java.util.List;
+
+public class QuakeAdapter extends ArrayAdapter<Quake> {
+
+    public QuakeAdapter(@NonNull Context context, @NonNull List<Quake> objects) {
+        super(context, 0, objects);
+    }
+}
+
