7250e6a68906c94ce583096d916f4a35b5a88d66
hd1801 <hardikdhawan0108@gmail.com>
2021-04-26 19:46:51 +0530

    updated array adapter

--- ud843-QuakeReport-starting-point/app/src/main/java/com/example/android/quakereport/QuakeAdapter.java
+++ ud843-QuakeReport-starting-point/app/src/main/java/com/example/android/quakereport/QuakeAdapter.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.WindowInsetsAnimationControlListener;
 import android.widget.ArrayAdapter;
 import android.widget.TextView;
 
@@ -16,6 +17,25 @@ public class QuakeAdapter extends ArrayAdapter<Quake> {
 
     public QuakeAdapter(@NonNull Context context, @NonNull List<Quake> objects) {
         super(context, 0, objects);
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+        View QuakeView = convertView;
+        if(QuakeView==null)
+            QuakeView = LayoutInflater.from(getContext()).inflate(R.layout.list_item,parent,false);
+
+        Quake quake= getItem(position);
+        TextView date =(TextView)QuakeView.findViewById((R.id.Date_text_view));
+        TextView location =(TextView)QuakeView.findViewById((R.id.Location_text_view));
+        TextView magnitude =(TextView)QuakeView.findViewById((R.id.magnitude_text_view));
+
+        date.setText(quake.getmDate());
+        location.setText(quake.getmLocation());
+        magnitude.setText(quake.getmMagnitude());
+
+        return QuakeView;
     }
 }
 
